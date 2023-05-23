ae3f27b24b79509fa8a44072981acb269c912186
kirisaki-vk <tolotrman.corp@gmail.com>
2023-05-23 17:49:33 +0300

    chore: used YAML loops instead of a long list

--- generate_playbook.py
+++ generate_playbook.py
@@ -14,16 +14,18 @@ args_parser.add_argument("-t", "--target", help='''Hosts name''', required=True)
 args_parser.add_argument("-i", "--input", help='''File containing the list to parse''', required=True)
 args_parser.add_argument("-o", "--output", help='''Output filename (Default 'output.ansible.yaml')''', required=False,
                          default="./output.ansible.yaml")
+args_parser.add_argument("-m", "--template", help='''Path to the configuration template. Variables are "port", "link", "path", "subpath"''', required=True)
 args_parser.add_argument("-u", "--user", help='''User to log in (Default is 'root')''', required=False, default="root")
 args_parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1')
 
 args = args_parser.parse_args()
 
 yaml_header = "---"
-yaml_data = {
+yaml_data = [{
     'name': 'Deploying hosts',
     'host': f'{args.target}',
     'remote_user': f'{args.user}',
+    'become': True,
     'tasks': [
         {
             'name': 'Check if apache is installed and in the lastest version',
@@ -33,7 +35,7 @@ yaml_data = {
             }
         }
     ]
-}
+}]
 
 
 def parse_list(file):
@@ -42,30 +44,38 @@ def parse_list(file):
 
     return [urlparse(line.strip()).netloc for line in lines]
 
-
-def generate_task(list):
+def generate_list(list):
     port_number = 49152
-    for link in list:
-        port_number += 1
-        path = link
-        subpath = link
+    yaml_data[0]['tasks'].append({
+        'name': 'Deploying hosts',
+        'vars': {
+            'links': list
+        },
+        'ansible.builtin.file': {
+            'path': "/var/{{  link.split('.')[0] }}/{{ link.split('.')[1] }}",
+            'state': 'directory',
+            'mode' : '0755'
+        },
+        'ansible.builtin.copy': {
+            'src': args.template,
+            'dest': '/etc/apache2/sites-available/{{ link }}.conf',
+            'owner': 'root',
+            'group': 'root',
+            'mode': '0644'
+        },
+        'ansible.builtin.file': {
+            'src': '/etc/apache2/sites-available/{{ link }}.conf',
+            'dest': '/etc/apache2/sites-enabled/{{ link }}.conf',
+            'state': 'link'
+        }
+        ,
+        'loop': '{{ links }}',
+        'loop_control': {
+            'loop_var': 'link'
+        }
+    })
 
-        splitted_link = link.split()
-        yaml_data['tasks'].append({
-            'name': f'Deploying {link}',
-            'vars': {
-                'link': link,
-                'port': port_number,
-                'path': path,
-                'subpath': subpath
-            },
-            'ansible.builtin.template': {
-                'src': './template/Apache2_config.template.j2',
-                'dest': f'/etc/apache2/sites-available/{link}.conf'
-            }
-        })
-
-generate_task(parse_list('test.txt'))
+generate_list(parse_list('test.txt'))
 
 with open(args.output, 'w') as output:
-    yaml.dump(yaml_data, output, sort_keys=False, explicit_start=True)
+    yaml.dump(yaml_data, output, sort_keys=False, explicit_start=True, Dumper=yaml.CDumper)
